4672d4d3c3f06383dd2e852a66ff93f15c16408c
xxyexx <313140949@qq.com>
2015-08-18 22:47:13 +0800

    新增用户实体类说明

--- pocc/src/edu/scnu316/entity/User.java
+++ pocc/src/edu/scnu316/entity/User.java
@@ -41,9 +41,6 @@ public class User implements Cloneable{
 	private String age;
 	private int city_id;
 	private String icon_url;
-
-	
-	
 	
 	
 	public User(){
@@ -63,60 +60,32 @@ public class User implements Cloneable{
 	public int getUser_id() {
 		return user_id;
 	}
-
-
 	public String getUser_account() {
 		return user_account;
 	}
-
-
-
 	public void setUser_account(String user_account) {
 		this.seq = Integer.parseInt(user_account.substring(user_account.length()-4));
 		this.user_account = user_account;
 	}
-
-
-
 	public String getUsername() {
 		if (username == null) return "";
 		return username;
 	}
-
-
-
 	public void setUsername(String username) {
 		if (username==null) username = "";
 		this.username = username;
 	}
-
-
-
 	public String getPassword() {
 		return password;
 	}
-
-
-
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
-
-
 	public String getLock_mode() {
 		return lock_mode;
 	}
 
-	/**
-	 * 返回锁定状态对应的中文
-	 * @return 有效、临时锁定或永久锁定
-	 */
-	public String getLock_modeToCN() {
-		if ("LOCK_TEMP".equals(this.lock_mode)) return "临时锁定";
-		if ("LOCK_EVER".equals(this.lock_mode)) return "永久锁定";
-		return "有效";
-	}
+	
 	
 	/**
 	 * 锁定状态
@@ -135,156 +104,84 @@ public class User implements Cloneable{
 		this.lock_mode = mode;
 	}
 	
-
-
-
-	/**
-	 * @return 单位名称
-	 */
 	public String getUnit_name() {
 		if (unit_name == null) return "";
 		return unit_name;
 	}
-
-
-
 	public void setUnit_name(String unit_name) {
 		if (unit_name==null) unit_name = "";
 		this.unit_name = unit_name;
 	}
-
-
-
 	public Timestamp getRent_start() {
 		return rent_start;
 	}
-	public String getRent_start(String formate){
-		return StringUtil.TimestampToString(rent_start, formate);
-	}
-
-
+	
 	public void setRent_start(Timestamp rent_start) {
 		this.rent_start = rent_start;
 	}
-
-
-
 	public Timestamp getRent_end() {
 		return rent_end;
 	}
-	public String getRent_end(String formate){
-		return StringUtil.TimestampToString(rent_end, formate);
-	}
 	
-
 	public void setRent_end(Timestamp rent_end) {
 		this.rent_end = rent_end;
 	}
-
-
 	public double getPrice() {
 		return price;
 	}
-
-
-
 	public void setPrice(double price) {
 		this.price = price;
 	}
-
-
-
 	public Timestamp getPay_date() {
 		return pay_date;
 	}
-	public String getPay_date(String formate){
-		return StringUtil.TimestampToString(pay_date, formate);
-	}
-
-
-
+	
 	public void setPay_date(Timestamp pay_date) {
 		this.pay_date = pay_date;
 	}
-
-
-
 	public String getOperator() {
 		return operator;
 	}
-
-
-
 	public void setOperator(String operator) {
 		this.operator = operator;
 	}
-
-
-
 	public String getSave_url() {
 		return save_url;
 	}
-
-
-
 	public void setSave_url(String save_url) {
 		this.save_url = save_url;
 	}
-
-
-
 	public Timestamp getLast_login() {
 		return last_login;
 	}
-	public String getLast_login(String formate) {
-		return StringUtil.TimestampToString(last_login, formate);
-	}
-
-
-
+	
 	public void setLast_login(Timestamp last_login) {
 		this.last_login = last_login;
 	}
-
-
-
 	public String getLast_ip() {
 		return last_ip;
 	}
-
-
-
 	public void setLast_ip(String last_ip) {
 		this.last_ip = last_ip;
 	}
-
-
-
 	public String getRemark() {
 		return remark;
 	}
-
-
-
 	public void setRemark(String remark) {
 		this.remark = remark;
 	}
-
-
-
 	public int getSeq() {
 		return seq;
 	}
-
-
-
 	public void setSeq(int seq) {
 		this.seq = seq;
-	}
-
+	}	
 	
 	
-	
+	/* 
+	 * 复制当前对象，返回对象的克隆
+	 * @see java.lang.Object#clone()
+	 */
 	@Override
 	public Object clone(){
 		User u = null;
@@ -295,69 +192,100 @@ public class User implements Cloneable{
 		}
 		return u;
 	}
-
-
-
 	public String getSex() {
 		return sex;
 	}
-
-
-
 	public void setSex(String sex) {
 		
 		this.sex = sex;
 	}
-
-
-
 	public String getAge() {
 		return age;
 	}
-
-
-
 	public void setAge(String age) {
 		this.age = age;
 	}
-
-
-
 	public int getCity_id() {
 		return city_id;
 	}
+	public void setCity_id(int city_id) {
+		this.city_id = city_id;
+	}
+	public String getIcon_url() {
+		return icon_url;
+	}
+	public void setIcon_url(String icon_url) {
+		this.icon_url = icon_url;
+	}
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
 	
+	
+	
+////////////////////////////////////////////////////////////////////////
+//**************            注意看!!!          **********************///
+///////////////////////////////////////////////////////////////////////
+//  以下为具有转化的属性，不属于实体属性，但是可以作为实体属性可以获取到     ///
+//                     或者具有其他转化方法                                                       ///
+/////////////////////////////////////////////////////////////////////
+
+	
+////////////////////////////////////////////////////////////////////
+///   1.中文类：返回属性对应中文字符                                                 ********///
+	
+	/**
+	 * 返回锁定状态对应的中文
+	 * @return 有效、临时锁定或永久锁定
+	 */
+	public String getLock_modeToCN() {
+		if ("LOCK_TEMP".equals(this.lock_mode)) return "临时锁定";
+		if ("LOCK_EVER".equals(this.lock_mode)) return "永久锁定";
+		return "有效";
+	}	
+///***************************************************************///
+	
+		
+////////////////////////////////////////////////////////////////////
+///   2.日期类：返回值进行日期的转换                                                 ********///
+		
+	public String getRent_start(String formate){
+		return StringUtil.TimestampToString(rent_start, formate);
+	}
+	public String getLast_login(String formate) {
+		return StringUtil.TimestampToString(last_login, formate);
+	}
+	public String getRent_end(String formate){
+		return StringUtil.TimestampToString(rent_end, formate);
+	}	
+	public String getPay_date(String formate){
+		return StringUtil.TimestampToString(pay_date, formate);
+	}
+	
+///***************************************************************///
+	
+	
+////////////////////////////////////////////////////////////////////
+///   3.城市类：返回属性对应城市、省份等                                          ********///
+
+	/**
+	 * 返回用户所属城市名
+	 * @return 如"广州市"
+	 */
 	public String getCity_name(){		
 		return CityUtil.getCity_name(this.city_id);
 	}
 	
+	/**
+	 * 返回用户所属城市名
+	 * @return 如"广东省"
+	 */
 	public String getProvince_name(){
 		return CityUtil.getProvince_name(this.city_id);
 	}
+	
 
-
-
-	public void setCity_id(int city_id) {
-		this.city_id = city_id;
-	}
-
-
-
-	public String getIcon_url() {
-		return icon_url;
-	}
-
-
-
-	public void setIcon_url(String icon_url) {
-		this.icon_url = icon_url;
-	}
-
-
-
-	public void setUser_id(int user_id) {
-		this.user_id = user_id;
-	}
-
+///***************************************************************///	
+////////////////////////////////////////////////////////////////////
 
 }
